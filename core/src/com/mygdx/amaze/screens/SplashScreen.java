88c4ac8f7e9d874a65df6c116bb0c6ab52db6034
randolphwong <wwk.randolph@gmail.com>
2016-04-10 12:49:04 +0800

    Fix image sizing issue at splash screen.

--- core/src/com/mygdx/amaze/screens/SplashScreen.java
+++ core/src/com/mygdx/amaze/screens/SplashScreen.java
@@ -1,8 +1,10 @@
 package com.mygdx.amaze.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -14,6 +16,8 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.amaze.AmazeGame;
 
 /**
@@ -22,6 +26,9 @@ import com.mygdx.amaze.AmazeGame;
 public class SplashScreen implements Screen{
 
     private AmazeGame game;
+
+    public OrthographicCamera camera;
+    public Viewport viewport;
 
     // stage ui
     private Stage stage;
@@ -36,37 +43,34 @@ public class SplashScreen implements Screen{
         this.game = game;
         time = TimeUtils.millis();
         System.out.println("time is: " + time);
-        // set up a stage for displaying button
-        stage = new Stage(new FillViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
-        Gdx.input.setInputProcessor(stage);
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(500, 500, camera);
+        camera.position.x = 250;
+        camera.position.y = 250;
 
-        backgroundImg = new Texture(Gdx.files.internal("startScreen/splashScreen_1.png"));
+        backgroundImg = new Texture(Gdx.files.internal("startScreen/splashScreen_2.png"));
         backgroundSet = new Sprite(backgroundImg);
-        backgroundSet.setScale(Gdx.graphics.getWidth() / backgroundImg.getWidth(), Gdx.graphics.getHeight() / backgroundSet.getHeight());
-
-
-
+        backgroundSet.setSize(500, 500);
     }
 
     public void update(float delta) {
-
+        camera.update();
     }
 
 
 
     @Override
     public void render(float delta) {
-        update(delta);
         // clear the screen
-        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        update(delta);
+        viewport.apply();
+        game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
-        game.batch.draw(backgroundImg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        backgroundSet.draw(game.batch);
         game.batch.end();
-
-        stage.act(delta);
-        stage.draw();
 
         if (TimeUtils.millis()>time + 5000) {
             game.setScreen(new MainScreen(game));
@@ -79,14 +83,15 @@ public class SplashScreen implements Screen{
     @Override
     public void dispose() {
         font.dispose();
-        stage.dispose();
+        //stage.dispose();
         backgroundImg.dispose();
     }
 
     @Override
     public void resize(int width, int height) {
         // centre the camera on the stage on resize
-        stage.getViewport().update(width, height, true);
+        //stage.getViewport().update(width, height, true);
+        viewport.update(width, height);
     }
 
     @Override
